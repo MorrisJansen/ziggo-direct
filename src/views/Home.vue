a58ae81d23fccd6bedf35e825fddc70ac2842851
MorrisJansen <morris8jansen@hotmail.com>
2024-09-26 08:30:39 +0200

    desktop home is af nog wel feedback vragen en even ss naast het design zetten

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,20 +1,15 @@
 <script>
 export default {
     mounted() {
-        // Functie om te detecteren of de browser Safari is
         const isSafari = () => {
             const ua = navigator.userAgent;
             const safari = ua.includes('Safari') && !ua.includes('Chrome');
-            console.log('Is Safari detected?', safari);
             return safari;
         };
 
         if (isSafari()) {
             document.body.classList.add('safari');
-            console.log('Safari class added to body'); 
-        } else {
-            console.log('Not Safari');
-        }
+        } 
     },
     methods: {
         goToPage1() {
@@ -55,7 +50,7 @@ export default {
         </div>
   
         <div class="afbeelding-3-navbar">
-          <img src="/public/ziggo-nav.png" alt="ziggo">
+          <img src="/public/ziggo-logo.png" alt="ziggo">
         </div>
       </div>
   
@@ -119,13 +114,84 @@ export default {
 
 
 
+            <div class="container-afbeeldingen-en-prijs">
+                <!-- Prijzen SVG -->
+                <svg class="prijzen-prijs-tv" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 56 56" fill="none">
+                  <circle cx="27.9047" cy="27.9047" r="27.9047" fill="#49B7AC"/>
+                  <text x="29" y="20" font-family="DM Sans" font-size="8" fill="white" text-anchor="middle">t.w.v.</text>
+                  <text x="29" y="35" font-family="DM Sans" font-size="12" font-weight="700" fill="white" text-anchor="middle">€699,-</text>
+                </svg>
 
-            <div class="container-afbeelding-home">
+                <svg class="prijzen-prijs-bol" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 56 56" fill="none">
+                    <circle cx="27.9047" cy="27.9047" r="27.9047" fill="#49B7AC"/>
+                    <text x="29" y="20" font-family="DM Sans" font-size="8" fill="white" text-anchor="middle">t.w.v.</text>
+                    <text x="29" y="35" font-family="DM Sans" font-size="12" font-weight="700" fill="white" text-anchor="middle">€400,-</text>
+                  </svg>
+
+
+
+                <svg class="prijzen-prijs-ps" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 56 56" fill="none">
+                  <circle cx="27.9047" cy="27.9047" r="27.9047" fill="#49B7AC"/>
+                  <text x="29" y="20" font-family="DM Sans" font-size="8" fill="white" text-anchor="middle">t.w.v.</text>
+                  <text x="29" y="35" font-family="DM Sans" font-size="12" font-weight="700" fill="white" text-anchor="middle">€599,-</text>
+                </svg>
+      
+
                 <img class="pijl-naar-afbeelding" src="/public/pijl-naar-afbeelding.svg" alt="">
                 <img class="afbeelding-prijzen" src="/public/afbeelding-home-desk.png" alt="">
+              </div>
+
+        </div>
+
+
+
+
+
+
+
+
+        <div class="inhoud-sectie-2">
+
+            <div class="container-meer-over-meer-voordeel">
+                <div class="meer-over-meer-voordeel">
+                    Meer over MeerVoordeel
+                </div>
+
+                <img class="logo-mv" src="/public/meervoordeel-nav.svg" alt="meervoordeel">
+                <img class="logo-ziggo" src="/public/ziggo-logo.png" alt="ziggo">
+
+
             </div>
 
+            <hr class="lijn-sectie-2">
+        </div>
+      </div>
 
+
+      <div class="inhoud-sectie-3">
+        <div class="container-text-sectie-3">
+            <div class="text-sectie-3">
+                MeerVoordeel biedt diverse kortings- en voordeelprogramma's aan, aan consumenten in Nederland. In samenwerking met Ziggo, een van de grootste aanbieders van kabeltelevisie, internet en telefoniediensten in Nederland, biedt MeerVoordeel speciale voordelen en kortingen aan nieuwe Ziggo-klanten.
+            </div>
+    
+            <div class="dikke-text-sectie-3">
+                Onder andere door deze samenwerking kunnen wij jou mooie acties zoals deze bieden.
+            </div>
+        </div>
+
+
+
+        <div class="afbeelding-sectie-3">
+            <img class="afbeelding-sectie-3-2" src="/public/afbeelding-sectie-3.png" alt="">
+        </div>
+
+      </div>
+
+
+      <div class="footer-container">
+      <hr class="lijn-sectie-2">
+        <div class="footer-text">
+            *Meervoordeel.nl is een officiële partner van Ziggo. Deelname mogelijk tot en met 31 juli 2024.<br> Actievoorwaarden van toepassing.
         </div>
       </div>
 
@@ -196,7 +262,7 @@ export default {
 
   .achtergrond-sectie-1 {
     background: linear-gradient(90deg, #072148 0%, #40A59F 100%);
-    height: 50rem;
+    height: 45vw;
   }
 
   .inhoud-sectie-1 {
@@ -311,16 +377,32 @@ export default {
 
 
 
-.pijl-naar-afbeelding {
+
+
+.container-afbeeldingen-en-prijs {
+    width: 50vw;
+    max-width: 50%;
+    height: auto;
+    object-fit: contain;
+    margin-left: 50%!important;
     position: relative;
-    top: -800px;
-    right: -65px;
-    width: 7vw;
-    z-index: 1;
-  }
+    bottom: 30%;
+    display: flex;
+    justify-content: center;
+}
+
+.afbeelding-prijzen {
+    width: 100%;
+    height: auto;
+    max-width: 50vw;
+    object-fit: contain;
+    transition: transform 0.3s ease-in-out;
+}
 
 
-  .container-afbeelding-home {
+
+
+.container-afbeelding-home {
     width: 50vw;
     display: flex;
     justify-content: flex-end;
@@ -331,204 +413,188 @@ export default {
 }
 
 
-
-
 .container-afbeelding-home img {
     max-width: 100%;
     height: auto;
 }
 
-
-
-
-
-
-
-
-
-
-/* Basis styling voor 1920px en groter */
-.pijl-naar-afbeelding {
-    position: relative;
-    transform: translate(65px, -800px) scale(1);
-    width: 7vw;
+.afbeelding-prijzen {
     z-index: 1;
-    transition: transform 0.3s ease;
+    position: absolute;
+    right: 6%;
+    top: 5vw;
 }
 
-.container-afbeelding-home {
-    width: 50vw;
-    display: flex;
-    justify-content: flex-end;
-    align-items: flex-end;
-    margin-left: 45%!important;
+.pijl-naar-afbeelding {
+    position: absolute;
+    width: 7vw;
+    height: auto;
+    z-index: 1;
+    right: 97%;
+    top: 5vw;
+  }
+
+
+.prijzen-prijs-tv {
+    z-index: 99 !important;
+    position: absolute;
+    right: 26%;
+    top: 4vw!important;
+    width: 15%;
+    transform: rotate(8deg);
+}
+
+.prijzen-prijs-bol {
+    z-index: 99 !important;
+    position: absolute;
+    right: 98%;
+    top: 33vw !important;
+    width: 15%;
+    transform: rotate(-8deg);
+}
+
+.prijzen-prijs-ps {
+    z-index: 99 !important;
+    position: absolute;
+    right: 33%;
+    top: 35vw !important;
+    width: 15%;
+    transform: rotate(8deg);
+}
+
+.container-afbeeldingen-en-prijs {
+    max-width: 50%;
+    height: auto;
+    object-fit: contain;
+    margin-left: 50%!important;
     position: relative;
-    bottom: 13%;
-    transition: transform 0.3s ease;
-}
+    bottom: 30%;
+  }
 
-.container-afbeelding-home img {
-    max-width: 100%;
+
+.overkoepelende-container-home {
+    width: 100vw;
+    height: auto;
+  }
+
+
+.afbeeldingen-prijzen{
+    width: 100%;
+    max-width: 50%!important;
     height: auto;
     object-fit: contain;
     transform: scale(1);
-    transition: transform 0.3s ease;
-}
-
-
-
-/* Media queries voor schermen kleiner dan 1920px */
-@media (max-width: 1740px) {
-    .pijl-naar-afbeelding {
-        transform: translate(26px, -20px) scale(0.85)!important;
-        width: 6vw;
-    }
-
-    .container-afbeelding-home {
-        width: 55vw;
-        margin-left: 40%!important;
-        bottom: 12%;
-    }
-
-    .container-afbeelding-home img {
-        transform: translate(20px, -66px) scale(0.85);
-    }
-}
-
-@media (max-width: 1620px) {
-    .pijl-naar-afbeelding {
-        transform: translate(65px, -42px) scale(0.8)!important;
-        width: 5vw;
-    }
-}
-
-
-@media (max-width: 1520px) {
-    .pijl-naar-afbeelding {
-        transform: translate(75px, -52px) scale(0.75)!important;
-        width: 5vw;
-    }
-
-    .achtergrond-sectie-1 {
-        height: 42rem;
-    }
-}
-
-@media (max-width: 1470px) {
-    .pijl-naar-afbeelding {
-        transform: translate(75px, -10px) scale(0.75)!important;
-    }
-
-    .afbeelding-prijzen {
-        transform: translate(20px, -100px) scale(0.85)!important;
-
-    }
-
-}
-
-
-@media (max-width: 1340px) {
-    .pijl-naar-afbeelding {
-        transform: translate(66px, 38px) scale(0.75)!important;
-
-    }
-}
-
-@media (max-width: 1240px) {
-    .pijl-naar-afbeelding {
-        transform: translate(66px, 95px) scale(0.75)!important;
-
-    }
-}
-
-@media (max-width: 1200px) {
-    .pijl-naar-afbeelding {
-        transform: translate(50px, 145px) scale(0.75)!important;
-
-    }
-    .achtergrond-sectie-1 {
-        height: 34rem;
-    }
-}
-
-
-@media (max-width: 1150px) {
-    .pijl-naar-afbeelding {
-        transform: translate(55px, 110px) scale(0.75)!important;
-
-    }
-}
-
-@media (max-width: 1100px) {
-    .pijl-naar-afbeelding {
-        transform: translate(40px, 136px) scale(0.75)!important;
-
-    }
-}
-
-@media (max-width: 1040px) {
-    .pijl-naar-afbeelding {
-        transform: translate(40px, 159px) scale(0.75)!important;
-
-    }
-}
-
-
-@media (max-width: 980px) {
-    .pijl-naar-afbeelding {
-        transform: translate(40px, 187px) scale(0.75)!important;
-
-    }
-    .achtergrond-sectie-1 {
-        height: 27rem;
-    }
-}
-
-
-@media (max-width: 920px) {
-    .pijl-naar-afbeelding {
-        transform: translate(40px, 215px) scale(0.75)!important;
-
-    }
-}
-
-
-@media (max-width: 870px) {
-    .pijl-naar-afbeelding {
-        transform: translate(30px, 245px) scale(0.75)!important;
-
-    }
-}
-
-
-@media (max-width: 790px) {
-    .pijl-naar-afbeelding {
-        transform: translate(25px, 285px) scale(0.75)!important;
-
-    }
-
-    .achtergrond-sectie-1 {
-        height: 22rem;
-    }
-}
-
-
-@media (max-width: 720px) {
-    .pijl-naar-afbeelding {
-        transform: translate(20px, 300px) scale(0.75)!important;
-
-    }
+    transition: transform 0.3s ease-in-out;
 }
 
 
 
 
+.inhoud-sectie-2 {
+    height: 19vw;
+    width: 100vw;
+    position: relative;
+    top: 20vw;
+}
+
+
+.container-meer-over-meer-voordeel {
+    padding-left: 10%!important;
+    display: flex;
+    height: 3vw;
+    text-align: left;
+}
+
+
+.meer-over-meer-voordeel {
+    color: #072249;
+    font-family: "DM Sans";
+    font-size: 1.7vw;
+    font-style: normal;
+    font-weight: 700;
+    line-height: 150%;
+    text-align: left;
+}
+
+.logo-mv {
+    margin-left: 47vw!important;
+}
+
+.logo-ziggo {
+    margin-left: 2vw!important;
+}
+
+.lijn-sectie-2 {
+    width: 85vw;
+    margin: 0 auto!important;
+    opacity: 0.3;
+    margin-top: 2vw!important;
+}
+
+.inhoud-sectie-3 {
+    position: relative;
+    top: 8vw;
+    margin-left: 10%!important;
+    display: flex;
+    width: 100vw
+}
 
 
 
+.text-sectie-3 {
+    width: 35vw;
+    position: relative;
+    top: 2vw;
+    text-align: left;
+    color: #072249;
+    font-family: "DM Sans";
+    font-size: 1.1vw;
+    font-style: normal;
+    font-weight: 400;
+    line-height: 2.2vw; 
+}
+
+.dikke-text-sectie-3 {
+    width: 35vw;
+    margin-top: 1.5vw!important;
+    font-weight: 700;
+    position: relative;
+    top: 2vw;
+    text-align: left;
+    color: #072249;
+    font-family: "DM Sans";
+    font-size: 1.1vw;
+    font-style: normal;
+    line-height: 2.2vw; 
+}
 
 
-  @media (max-width: 700px){
+.afbeelding-sectie-3-2 {
+    width: 33vw;
+    position: relative;
+    left: 14.6vw;
+}
+
+.footer-container {
+    position: relative;
+    top: 15vw;
+    padding-bottom: 2vw!important;
+}
+
+.footer-text {
+    margin-top: 2vw!important;
+    color: #C1C1C1;
+    text-align: center;
+    font-family: "DM Sans";
+    font-size: 0.9vw;
+    font-style: normal;
+    font-weight: 400;
+    line-height: 1.4vw;
+}
+
+
+
+@media (max-width: 700px){
     * {
         display: none;
     }
